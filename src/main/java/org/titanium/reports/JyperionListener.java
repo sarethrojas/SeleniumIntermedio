d6d7f773d8f0c2a612163878b79eeebc20f007dc
Sareth Rojas <miztlli.rojas@titaniumsolutions.com>
2018-10-10 13:24:09 -0500

    Reports & Video

--- src/main/java/org/titanium/reports/JyperionListener.java
+++ src/main/java/org/titanium/reports/JyperionListener.java
@@ -1,16 +1,15 @@
 package org.titanium.reports;
 
-
-import com.sun.javafx.font.FontFactory;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 import com.lowagie.text.*;
 import com.lowagie.text.Font;
 import com.lowagie.text.pdf.PdfAction;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
 import java.awt.*;
 import java.io.FileOutputStream;
 import java.io.IOException;
